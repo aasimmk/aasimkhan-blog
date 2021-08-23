7624d36449f7387103141b84822b8d64ce61bd2b
Aasim Khan <khan.aasim.88@gmail.com>
2021-08-23 17:54:39 +0530

    Update settings

--- src/myblog/settings/dev.py
+++ src/myblog/settings/dev.py
@@ -7,7 +7,17 @@ DEBUG = True
 SECRET_KEY = 'django-insecure-gqz-yupd(j%^&524bq@*fcqrr$37^v+(zzdrq2$sh)p%ioi=h%'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['*']
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
--- src/myblog/settings/production.py
+++ src/myblog/settings/production.py
@@ -6,3 +6,16 @@ try:
     from .local import *
 except ImportError:
     pass
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USERNAME"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT", 5432),
+    }
+}
